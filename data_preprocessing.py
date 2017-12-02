4750595b76e6375958d4a391155a3f64fc24aa75
Kritanu Singh <kritanu82@gmail.com>
2017-12-02 06:57:52 +0530

    Update and rename data_preproc.py to data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -16,14 +16,12 @@ X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:, 3].values
 
 #taking care of missing data
-
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 #encoding categorical variable, making dummy variables
-
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[ :, 0] = labelencoder_X.fit_transform(X[:, 0])
@@ -33,12 +31,10 @@ labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
 #splitting dataset into training and test
-
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
 #Feature Scaling
-
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
